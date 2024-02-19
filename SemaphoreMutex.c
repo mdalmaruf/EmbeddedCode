412079f22f00bcf2b1d53f1608c4130f080baf63
Md Al Maruf <32615553+mdalmaruf@users.noreply.github.com>
2024-02-19 17:43:25 -0500

    Update SemaphoreMutex.c

--- SemaphoreMutex.c
+++ SemaphoreMutex.c
@@ -1,4 +1,6 @@
-//Mutexes and semaphores are synchronization primitives used to manage access to shared resources and ensure data consistency in concurrent programming. Mutexes provide mutual exclusion, allowing only one thread to access a resource at a time. Semaphores, on the other hand, can allow a certain number of threads to access a resource concurrently, useful for controlling access to a pool of resources or for signaling between threads.
+//Mutexes and semaphores are synchronization primitives used to manage access to shared resources and ensure data consistency in concurrent programming. 
+//Mutexes provide mutual exclusion, allowing only one thread to access a resource at a time. 
+//Semaphores, on the other hand, can allow a certain number of threads to access a resource concurrently, useful for controlling access to a pool of resources or for signaling between threads.
 
 #include <pthread.h>
 
